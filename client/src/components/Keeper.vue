bc13b3cd1a55257d8469b74794b462da43c1348c
Yunkan <unknsoul@mail.ru>
2021-05-05 14:50:25 +0300

    add favicon + add keeper's services

--- client/src/components/Keeper.vue
+++ client/src/components/Keeper.vue
@@ -4,66 +4,104 @@
 			<h2>Еда:</h2>
 			<div class="btns-block">
 				<PrimaryButton title="Хлеб(5 м.)" v-on:click="buyService([
-								{ name: 'starve', value: 10 },
-								{ name: 'stamina', value: -10 },
-								{ name: 'money', value: -5 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: 10 },
+					{ name: 'stamina', value: -10 },
+					{ name: 'money', value: -5 }
+				])"></PrimaryButton>
 				<PrimaryButton title="Хлеб с колбасой(8 м.)" v-on:click="buyService([
-								{ name: 'starve', value: 25 },
-								{ name: 'stamina', value: -10 },
-								{ name: 'money', value: -8 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: 25 },
+					{ name: 'stamina', value: -10 },
+					{ name: 'money', value: -8 }
+				])"></PrimaryButton>
 				<PrimaryButton title="Курица(20 м.)" v-on:click="buyService([
-								{ name: 'starve', value: 40 },
-								{ name: 'stamina', value: -10 },
-								{ name: 'money', value: -20 },
-								{ name: 'rating', value: 1 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: 40 },
+					{ name: 'stamina', value: -10 },
+					{ name: 'money', value: -20 },
+					{ name: 'rating', value: 5 }
+				])"></PrimaryButton>
+				<PrimaryButton title="Жаренная свинина(35 м.)" v-on:click="buyService([
+					{ name: 'starve', value: 60 },
+					{ name: 'stamina', value: -10 },
+					{ name: 'money', value: -35 },
+					{ name: 'rating', value: 10 }
+				])"></PrimaryButton>
+				<PrimaryButton title="Роскошный стол(60 м.)" v-on:click="buyService([
+					{ name: 'starve', value: 100 },
+					{ name: 'stamina', value: -15 },
+					{ name: 'money', value: -60 },
+					{ name: 'rating', value: 15 }
+				])"></PrimaryButton>
 			</div>
 		</div>
 		<div class="container-block">
 			<h2>Отдых:</h2>
 			<div class="btns-block">
 				<PrimaryButton title="Сено(5 м.)" v-on:click="buyService([
-								{ name: 'starve', value: -5 },
-								{ name: 'stamina', value: 10 },
-								{ name: 'money', value: -5 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: -5 },
+					{ name: 'stamina', value: 10 },
+					{ name: 'money', value: -5 }
+				])"></PrimaryButton>
 				<PrimaryButton title="Кровать(8 м.)" v-on:click="buyService([
-								{ name: 'starve', value: -10 },
-								{ name: 'stamina', value: 25 },
-								{ name: 'money', value: -8 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: -10 },
+					{ name: 'stamina', value: 25 },
+					{ name: 'money', value: -8 }
+				])"></PrimaryButton>
 				<PrimaryButton title="Уютная кровать(15 м.)" v-on:click="buyService([
-								{ name: 'starve', value: -10 },
-								{ name: 'stamina', value: 40 },
-								{ name: 'money', value: -15 },
-								{ name: 'ratin', value: 1 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: -10 },
+					{ name: 'stamina', value: 40 },
+					{ name: 'money', value: -15 },
+					{ name: 'rating', value: 5 }
+				])"></PrimaryButton>
+				<PrimaryButton title="Кровать и ванна(30 м.)" v-on:click="buyService([
+					{ name: 'starve', value: -10 },
+					{ name: 'stamina', value: 60 },
+					{ name: 'money', value: -30 },
+					{ name: 'rating', value: 10 }
+				])"></PrimaryButton>
+				<PrimaryButton title="Целая комната(65 м.)" v-on:click="buyService([
+					{ name: 'starve', value: -10 },
+					{ name: 'stamina', value: 100 },
+					{ name: 'money', value: -65 },
+					{ name: 'rating', value: 15 }
+				])"></PrimaryButton>
 			</div>
 		</div>
 		<div class="container-block">
 			<h2>Самочувствие:</h2>
 			<div class="btns-block">
 				<PrimaryButton title="Бинт(5 м.)" v-on:click="buyService([
-								{ name: 'starve', value: -2 },
-								{ name: 'stamina', value: -2 },
-								{ name: 'money', value: -5 },
-								{ name: 'health', value: 10 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: -2 },
+					{ name: 'stamina', value: -2 },
+					{ name: 'money', value: -5 },
+					{ name: 'health', value: 10 }
+				])"></PrimaryButton>
 				<PrimaryButton title="Сиропчик(8 м.)" v-on:click="buyService([
-								{ name: 'starve', value: -2 },
-								{ name: 'stamina', value: -2 },
-								{ name: 'money', value: -8 },
-								{ name: 'health', value: 20 }
-							])"></PrimaryButton>
-				<PrimaryButton title="Лекарь(12 м.)" v-on:click="buyService([
-								{ name: 'starve', value: -2 },
-								{ name: 'stamina', value: -2 },
-								{ name: 'money', value: -12 },
-								{ name: 'health', value: 40 },
-								{ name: 'rating', value: 1 }
-							])"></PrimaryButton>
+					{ name: 'starve', value: -2 },
+					{ name: 'stamina', value: -2 },
+					{ name: 'money', value: -8 },
+					{ name: 'health', value: 20 }
+				])"></PrimaryButton>
+				<PrimaryButton title="Травник(12 м.)" v-on:click="buyService([
+					{ name: 'starve', value: -2 },
+					{ name: 'stamina', value: -2 },
+					{ name: 'money', value: -12 },
+					{ name: 'health', value: 40 },
+					{ name: 'rating', value: 5 }
+				])"></PrimaryButton>
+				<PrimaryButton title="Врач(28 м.)" v-on:click="buyService([
+					{ name: 'starve', value: -2 },
+					{ name: 'stamina', value: -2 },
+					{ name: 'money', value: -28 },
+					{ name: 'health', value: 60 },
+					{ name: 'rating', value: 10 }
+				])"></PrimaryButton>
+				<PrimaryButton title="Лечебные процедуры(50 м.)" v-on:click="buyService([
+					{ name: 'starve', value: -2 },
+					{ name: 'stamina', value: 10 },
+					{ name: 'money', value: -50 },
+					{ name: 'health', value: 100 },
+					{ name: 'rating', value: 15 }
+				])"></PrimaryButton>
 			</div>
 		</div>
 
@@ -85,6 +123,7 @@
 		.btns-block {
 			display: flex;
 			justify-content: center;
+			flex-flow: row wrap;
 		}
 	}
 </style>
